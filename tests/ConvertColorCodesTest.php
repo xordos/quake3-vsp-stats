d0942b6e19ec797c07ab1634d601178c149f097b
Martin Haberfellner <evilru@users.noreply.github.com>
2025-03-12 18:29:23 +0000

    ✅ Add unit tests for XP-specific color code conversions
    - Added `testXpConvertAllColorCodes` to test color code conversion for XP gametype.
    - Added `testXpConvertColorCodesWithSpecialChar` to test special character conversion for XP gametype.
    - Added `testXpConvertSpecialChars103` to test special character conversion for XP version 1.03.
    - Updated method names for Q3A tests to follow consistent naming convention.

--- tests/ConvertColorCodesTest.php
+++ tests/ConvertColorCodesTest.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../vsp-q3a.php'; // Include the VSPParserQ3A class
 
 class ConvertColorCodesTest extends TestCase
 {
-    public function testConvertAllQ3aColorCodes()
+    public function testQ3aConvertAllColorCodes()
     {
         $configData = ["gametype" => "q3a"];
         $statsAggregator = $this->createMock(StatsAggregator::class);
@@ -17,7 +17,8 @@ class ConvertColorCodesTest extends TestCase
         $expected = "`#FFFFFF`#FF0000Red `#00FF00Green `#FFFF00Blue `#4444FFYellow `#00FFFFCyan `#FF00FFMagenta `#FFFFFFWhite `#777777Gray";
         $this->assertEquals($expected, $parser->convertColorCodes($input));
     }
-    public function testConvertEmptyName()
+
+    public function testQ3aConvertEmptyName()
     {
         $configData = ["gametype" => "q3a"];
         $statsAggregator = $this->createMock(StatsAggregator::class);
@@ -29,7 +30,7 @@ class ConvertColorCodesTest extends TestCase
         $this->assertEquals($expected, $parser->convertColorCodes($input));
     }
 
-    public function testConvertColorCodesWithSpecialChar()
+    public function testQ3aConvertColorCodesWithSpecialChar()
     {
         $configData = ["gametype" => "q3a"];
         $statsAggregator = $this->createMock(StatsAggregator::class);
@@ -42,7 +43,7 @@ class ConvertColorCodesTest extends TestCase
         $this->assertEquals($expected, $parser->convertColorCodes($input));
     }
 
-    public function testConvertColorCodesWithHexColor()
+    public function testQ3aConvertColorCodesWithHexColor()
     {
         $configData = ["gametype" => "q3a"];
         $statsAggregator = $this->createMock(StatsAggregator::class);
@@ -53,6 +54,41 @@ class ConvertColorCodesTest extends TestCase
         $input = "^xFF0000Red";
         $expected = "`#FFFFFF`#FF0000Red";
         $this->assertEquals($expected, $parser->convertColorCodes($input));
+    }
+    public function testXpConvertAllColorCodes()
+    {
+        $configData = ["gametype" => "xp"];
+        $statsAggregator = $this->createMock(StatsAggregator::class);
+        $statsProcessor = $this->createMock(StatsProcessor::class);
+        $parser = new VSPParserQ3A($configData, $statsAggregator, $statsProcessor);
+
+        $input = "^1Red ^2Green ^3Blue ^4Yellow ^5Cyan ^6Magenta ^7White ^0Gray";
+        $expected = "`#e90000Red `#00dd24Green `#f5d800Blue `#2e61c8Yellow `#16b4a5Cyan `#f408f1Magenta `#efefefWhite `#555555Gray";
+        $this->assertEquals($expected, $parser->convertColorCodes($input));
+    }
+    public function testXpConvertColorCodesWithSpecialChar()
+    {
+        $configData = ["gametype" => "xp"];
+        $statsAggregator = $this->createMock(StatsAggregator::class);
+        $statsProcessor = $this->createMock(StatsProcessor::class);
+        $parser = new VSPParserQ3A($configData, $statsAggregator, $statsProcessor);
+
+        // Input that triggers the special character conversion
+        $input = "#23SpecialChar";
+        $expected = "`#efefef" . chr(hexdec('23')) . "SpecialChar";
+        $this->assertEquals($expected, $parser->convertXPColorCodes($input));
+    }
+    public function testXpConvertSpecialChars103()
+    {
+        $configData = ["gametype" => "xp", "xp_version" => 103];
+        $statsAggregator = $this->createMock(StatsAggregator::class);
+        $statsProcessor = $this->createMock(StatsProcessor::class);
+        $parser = new VSPParserQ3A($configData, $statsAggregator, $statsProcessor);
+
+        // Input that triggers the special character conversion for XP version 1.03
+        $input = "+A#SpecialChar";
+        $expected = "`#efefef@SpecialChar";
+        $this->assertEquals($expected, $parser->convertXPColorCodes($input));
     }
 }
 
