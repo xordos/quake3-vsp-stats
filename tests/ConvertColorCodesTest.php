de5787034ea90ecc80f44be08cc2774973e2c0ae
Martin Haberfellner <evilru@users.noreply.github.com>
2025-04-11 11:52:11 +0000

    ♻️ Refactor ConvertColorCodesTest to use invokeMethod for cleaner assertions

--- tests/ConvertColorCodesTest.php
+++ tests/ConvertColorCodesTest.php
@@ -29,81 +29,100 @@ class ConvertColorCodesTest extends TestCase
         $this->xpParser103 = new VSPParserQ3A($xpConfig103, self::$gameDataProcessor, self::$playerSkillProcessor);
     }
 
+    private function invokeMethod($parser, $methodName, array $parameters = [])
+    {
+        $reflection = new ReflectionClass(get_class($parser));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method->invokeArgs($parser, $parameters);
+    }
+
     public function testQ3aConvertAllColorCodes()
     {
         $input = "^1Red ^2Green ^3Blue ^4Yellow ^5Cyan ^6Magenta ^7White ^0Gray";
         $expected = "`#FFFFFF`#FF0000Red `#00FF00Green `#FFFF00Blue `#4444FFYellow `#00FFFFCyan `#FF00FFMagenta `#FFFFFFWhite `#777777Gray";
-        $this->assertEquals($expected, $this->q3aParser->convertColorCodes($input));
+        $actual = $this->invokeMethod($this->q3aParser, 'convertColorCodes', [$input]);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testQ3aConvertEmptyName()
     {
         $input = "";
         $expected = " ";
-        $this->assertEquals($expected, $this->q3aParser->convertColorCodes($input));
+        $actual = $this->invokeMethod($this->q3aParser, 'convertColorCodes', [$input]);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testQ3aConvertColorCodesWithSpecialChar()
     {
         $input = "^fSpecialChar";
         $expected = "`#FFFFFFSpecialChar";
-        $this->assertEquals($expected, $this->q3aParser->convertColorCodes($input));
+        $actual = $this->invokeMethod($this->q3aParser, 'convertColorCodes', [$input]);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testQ3aConvertColorCodesWithHexColor()
     {
         $input = "^xFF0000Red";
         $expected = "`#FFFFFF`#FF0000Red";
-        $this->assertEquals($expected, $this->q3aParser->convertColorCodes($input));
+        $actual = $this->invokeMethod($this->q3aParser, 'convertColorCodes', [$input]);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testQ3aConvertAllListedColors()
     {
         $input = "^0Gray ^1Red ^2Green ^3Yellow ^4Blue ^5Cyan ^6Magenta ^7White";
         $expected = "`#FFFFFF`#777777Gray `#FF0000Red `#00FF00Green `#FFFF00Yellow `#4444FFBlue `#00FFFFCyan `#FF00FFMagenta `#FFFFFFWhite";
-        $this->assertEquals($expected, $this->q3aParser->convertColorCodes($input));
+        $actual = $this->invokeMethod($this->q3aParser, 'convertColorCodes', [$input]);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testXpConvertAllColorCodes()
     {
         $input = "^1Red ^2Green ^3Blue ^4Yellow ^5Cyan ^6Magenta ^7White ^0Gray";
         $expected = "`#e90000Red `#00dd24Green `#f5d800Blue `#2e61c8Yellow `#16b4a5Cyan `#f408f1Magenta `#efefefWhite `#555555Gray";
-        $this->assertEquals($expected, $this->xpParser->convertColorCodes($input));
+        $actual = $this->invokeMethod($this->xpParser, 'convertColorCodes', [$input]);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testXpConvertColorCodesWithSpecialChar()
     {
         $input = "#23SpecialChar";
         $expected = "`#efefef" . chr(hexdec('23')) . "SpecialChar";
-        $this->assertEquals($expected, $this->xpParser->convertXPColorCodes($input));
+        $actual = $this->invokeMethod($this->xpParser, 'convertXPColorCodes', [$input]);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testXpConvertSpecialChars103()
     {
         $input = "+A#SpecialChar";
         $expected = "`#efefef@SpecialChar";
-        $this->assertEquals($expected, $this->xpParser103->convertXPColorCodes($input));
+        $actual = $this->invokeMethod($this->xpParser103, 'convertXPColorCodes', [$input]);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testXpConvertEmptyName()
     {
         $input = "";
         $expected = "`#efefef";
-        $this->assertEquals($expected, $this->xpParser->convertColorCodes($input));
+        $actual = $this->invokeMethod($this->xpParser, 'convertColorCodes', [$input]);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testXpConvertColorCodesWithHexColor()
     {
         $input = "^xFF0000Red";
         $expected = "`#FF0000Red";
-        $this->assertEquals($expected, $this->xpParser->convertColorCodes($input));
+        $actual = $this->invokeMethod($this->xpParser, 'convertColorCodes', [$input]);
+        $this->assertEquals($expected, $actual);
     }
 
     public function testXpConvertAllListedColors()
     {
         $input = "^0Gray ^1Red ^2Green ^3Yellow ^4Blue ^5Cyan ^6Magenta ^7White";
         $expected = "`#555555Gray `#e90000Red `#00dd24Green `#f5d800Yellow `#2e61c8Blue `#16b4a5Cyan `#f408f1Magenta `#efefefWhite";
-        $this->assertEquals($expected, $this->xpParser->convertColorCodes($input));
+        $actual = $this->invokeMethod($this->xpParser, 'convertColorCodes', [$input]);
+        $this->assertEquals($expected, $actual);
     }
 }
 
