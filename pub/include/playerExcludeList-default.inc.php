45c959c2ccc8baa718d721e226d51a282e012e9f
yourname <your@email.com>
2025-08-10 00:21:33 -0500

    set bot to exclusion List

--- pub/include/playerExcludeList-default.inc.php
+++ pub/include/playerExcludeList-default.inc.php
@@ -10,7 +10,39 @@
   // Specify IPs   if tracking by ip
 
   $player_exclude_list=array(
-      //'' // uncomment the line to exclude all the bots when tracking by guid in xp
+    //'' // uncomment the line to exclude all the bots when tracking by guid in xp
+    "^1A^2n^3a^4r^5k^6i"
+    ,"Angel"
+    ,"Biker"
+    ,"Bitterman"
+    ,"Bones"
+    ,"Cadavre"
+    ,"Crash"
+    ,"Daemia"
+    ,"Doom"
+    ,"Gorre"
+    ,"Grunt"
+    ,"Hossman"
+    ,"Hunter"
+    ,"Keel"
+    ,"Klesk"
+    ,"Lucy"
+    ,"Major"
+    ,"Mynx"
+    ,"Orbb"
+    ,"Patriot"
+    ,"Phobos"
+    ,"Ranger"
+    ,"Razor"
+    ,"Sarge"
+    ,"Slash"
+    ,"Sorlag"
+    ,"Stripe"
+    ,"TankJr"
+    ,"Uriel"
+    ,"Visor"
+    ,"Wrack"
+    ,"Xaero"
   );
 
 ?>
